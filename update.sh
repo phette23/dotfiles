b41f3dbb115c8a882924f62715793133c8f9edb9
Eric Phetteplace <phette23@gmail.com>
2012-11-25 17:43:13 -0500

    push .gitconfig updates to ~ & prepend user settings

--- update.sh
+++ update.sh
@@ -3,4 +3,10 @@
 for file in .{path,bash_prompt,exports,aliases,functions,extra}; do
     cp "$file" ~/"$file"
 done
+
+# concatenate dotfiles .gitconfig w/ user-specific settings in ~/.user-gitconfig
+if [ -f .gitconfig ] && [ -f ~/.user-gitconfig ]; then
+    cat ~/.user-gitconfig .gitconfig > ~/.gitconfig
+fi
+
 source ~/.bash_profile
