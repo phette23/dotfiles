111aa62ce1a0457809307bc60d6d2fc0e0842005
Eric Phetteplace <phette23@gmail.com>
2013-03-20 15:27:38 -0400

    update.sh: add bower bash completion

--- update.sh
+++ update.sh
@@ -9,4 +9,9 @@ if [ -f .gitconfig ] && [ -f ~/.user-gitconfig ]; then
     cat ~/.user-gitconfig .gitconfig > ~/.gitconfig
 fi
 
+# if bower's available, add it's completion script to .bash_profile
+if command -v "bower" &> /dev/null 2>&1; then
+    bower completion >> ~/.bash_profile
+fi
+
 source ~/.bash_profile
